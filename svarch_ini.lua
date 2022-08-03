2d85597bb34d0d8a634b8ac3c7e62446a5043830
sv <nevidprog@gmail.com>
2022-08-03 13:27:48 +0300

    1

---
+++ svarch_ini.lua
@@ -0,0 +1,45 @@
+
+Archs={
+	{	
+		dir="./test2/",
+		compress=0,
+		maxsz=0,	--KBytes  1024 (MB) 1024*1024 (GB)
+		maxfilesz=0, --KBytes
+		maxdur=1,   --minutes
+	},
+	{
+		dir="./ipatest/",
+		compress=0,
+		maxsz=100000,
+		maxfilesz=10000,
+		maxdur=0,
+	},
+	{
+		dir="./sv3wtest/",
+		compress=1,
+		maxsz=1000000,
+		maxfilesz=1000,
+		maxdur=0,
+	},
+	{
+		dir="./sv3wtest1/",
+		compress=0,
+		maxsz=1000000,
+		maxfilesz=150000,
+		maxdur=0,
+	},
+	{
+		dir="./wrktest/",
+		compress=0,
+		maxsz=100000,
+		maxfilesz=10000,
+		maxdur=0,
+	},
+	{
+		dir="./lbltest/",
+		compress=0,
+		maxsz=1000,
+		maxfilesz=100,
+		maxdur=0,
+	},
+}
